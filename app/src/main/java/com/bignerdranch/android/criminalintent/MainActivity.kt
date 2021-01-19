4415ab3c10d98b5f9dab93ea7cc6afbe1fc29029
Artur <torbogoshev@yandex.ru>
2021-01-19 08:02:29 +0200

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.criminalintent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *  Torbogoshev Artur
+ *  last update: 199
+ *
+ **/
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
