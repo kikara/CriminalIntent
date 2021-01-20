167b1f9b1d8454c797ad8a415444b8040bdf4de6
Artur <torbogoshev@yandex.ru>
2021-01-20 07:30:05 +0200

    first fragment CrimeFragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.bignerdranch.android.criminalintent
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  *  Torbogoshev Artur
- *  last update: 199
+ *  last update: 206
  *
  **/
 
@@ -14,5 +16,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Сначала запрашивается у FragmentManager фрагмент
+       val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+//        Если его нет, то создаем новый экземпляр и новую транзакцию
+        if(currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
     }
 }
